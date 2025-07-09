da5505c8f49c771c334b0d2d57cfdde8213d819b
lixiao <lixiao@nb-666.com>
2025-07-09 14:30:46 +0800

    修改依赖

---
+++ ble_event_lib/src/main/java/com/nrmyw/ble_event_lib/statu/BleStatuEventObserver.java
@@ -0,0 +1,15 @@
+package com.nrmyw.ble_event_lib.statu;
+
+
+/**
+ * Created by lixiao on 2017/5/12.
+ * about:
+ */
+
+public interface BleStatuEventObserver {
+
+    public  void sendBleStatu(BleStatu bleStatu,Object... objects);
+
+
+
+}
---
+++ ble_event_lib/src/main/java/com/nrmyw/ble_event_lib/statu/BleStatuEventSubscriptionSubject.java
@@ -0,0 +1,47 @@
+package com.nrmyw.ble_event_lib.statu;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BleStatuEventSubscriptionSubject implements BleStatuEventSubject {
+
+    private List<BleStatuEventObserver> observers;
+    private static BleStatuEventSubscriptionSubject subscriptionSubject;
+
+    private BleStatuEventSubscriptionSubject() {
+        observers = new ArrayList<>();
+    }
+
+    public static BleStatuEventSubscriptionSubject getInstance() {
+        if (subscriptionSubject == null) {
+            synchronized (BleStatuEventSubscriptionSubject.class) {
+                if (subscriptionSubject == null)
+                    subscriptionSubject = new BleStatuEventSubscriptionSubject();
+            }
+        }
+        return subscriptionSubject;
+
+    }
+
+    @Override
+    public void attach(BleStatuEventObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void detach(BleStatuEventObserver observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void sendBleStatu(BleStatu bleStatu, Object... objects) {
+        for (BleStatuEventObserver observer:observers){
+            observer.sendBleStatu(bleStatu,objects);
+        }
+    }
+
+
+}
